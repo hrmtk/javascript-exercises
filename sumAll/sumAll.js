5924b5fc8fbdf9c74ecc6846f5b99bdb3ca1833c
hrmtk <50558852+hrmtk@users.noreply.github.com>
2020-08-04 13:31:18 +0900

    Update sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let sum = 0;
+  if (Number.isInteger(x) && Number.isInteger(y) && x >= 0 && y >= 0) {
+    const start = Math.min(x, y);
+    const end = Math.max(x, y);
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+  } else {
+    return 'ERROR';
+  }
+  return sum;
 }
 
 module.exports = sumAll
