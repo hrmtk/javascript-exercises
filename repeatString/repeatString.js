732bdf9c2a144c23427071b2020d03b1e84d5dcc
hrmtk <50558852+hrmtk@users.noreply.github.com>
2020-08-04 09:39:33 +0900

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  if (num < 0) {
+    return 'ERROR'
+  }
 
+  let message = '';
+  for (let i = 0; i < num; i++) {
+    message += str;
+  }
+  return message;
 }
 
 module.exports = repeatString
