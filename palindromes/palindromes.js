5e60ddffd64ca1a752c117160a7ee5bdff95a9e7
hrmtk <50558852+hrmtk@users.noreply.github.com>
2020-08-08 10:50:23 +0900

    Update palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  const string = str.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+  const reverseStr =  string.split('').reverse().join('');
+  return string == reverseStr;
 }
 
 module.exports = palindromes
