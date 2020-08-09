98dd01dd6b1b0770a125da83449fac81bf8bcaf6
hrmtk <50558852+hrmtk@users.noreply.github.com>
2020-08-09 09:52:04 +0900

    caesar.js

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,20 @@
-const caesar = function() {
+const caesar = function(str, key) {
 
+  if (key < 0) return caesar(str, key + 26);
+  let ans = '';
+
+  for (let i = 0; i < str.length; i++) {
+    let code = str.charCodeAt(i);
+    if (code >= 65 && code <= 90)  {
+      ans += String.fromCharCode(((code - 65 + key) % 26) + 65);
+    } else if (code >= 97 && code <= 122) {
+      ans += String.fromCharCode(((code - 97 + key) % 26) + 97);
+    } else {
+      ans += str[i];
+    }
+  }
+
+  return ans;
 }
 
 module.exports = caesar
